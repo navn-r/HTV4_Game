ce931eadb70873298bafa070d838c4910dee68cd
Navinn Ravindaran <home@Navinns-MacBook-Pro.local>
2020-02-15 03:19:44 -0500

    init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float mspdX = 30f;
+    public float mspdY = 1f;
+    float velY, velX;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        velX = Input.GetAxisRaw("Horizontal");
+        velY = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(velX * mspdX, velY * mspdY);
+        
+    }
+}
