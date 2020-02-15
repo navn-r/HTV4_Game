35638263cee64a6641feb4b74cff695aa89e91a6
Navinn Ravindaran <home@Navinns-MacBook-Pro.local>
2020-02-15 08:10:01 -0500

    ur mom

---
+++ Assets/Scripts/Player1_Move.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1_Move : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float velY = 17f;
+    public float velX = 5f;
+    public bool isGrounded = true;
+    public LayerMask mask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if(Input.GetKeyDown(KeyCode.W) && isGrounded) {
+            rb.velocity = Vector2.up * velY;
+        } 
+        if(isGrounded) {
+            if(Input.GetKey(KeyCode.A)) {
+                rb.velocity = Vector2.left * velX;
+            } if(Input.GetKey(KeyCode.D)) {
+                rb.velocity = Vector2.right * velX;
+            }  
+        } else {
+            if(Input.GetKeyDown(KeyCode.A)) {
+                rb.velocity = Vector2.left * velX;
+            } if(Input.GetKeyDown(KeyCode.D)) {
+                rb.velocity = Vector2.right * velX;
+            }
+        }
+        
+    }
+}
+
+
---
+++ Assets/Scripts/Player2_Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2_Move : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float velY = 17f;
+    public float velX = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if(Input.GetKeyDown(KeyCode.I) || Input.GetKeyDown(KeyCode.UpArrow)) {
+            rb.velocity = Vector2.up * velY;
+        } if(Input.GetKeyDown(KeyCode.J) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+            rb.velocity = Vector2.left * velX;
+        } if(Input.GetKeyDown(KeyCode.L) || Input.GetKeyDown(KeyCode.RightArrow)) {
+            rb.velocity = Vector2.right * velX;
+        }
+        
+    }
+}
